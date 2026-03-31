d2baf9e9a7d3138d8d3078a01de5c725d5c4e489
Apec <apecchintathum@gmail.com>
2026-03-31 10:06:43 +0700

    FIx// banner min-height

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -189,7 +189,7 @@ export default function CoursePage() {
                 />}
 
                 <div className="flex flex-col lg:px-50 md:px-20 px-5 w-full h-fit min-h-[500px]">
-                    <div className="relative rounded-b-lg w-full h-[190px] bg-primary overflow-hidden">
+                    <div className="relative rounded-b-lg w-full min-h-[190px] bg-primary overflow-hidden">
                         {
                             bannerUrl && <img src={bannerUrl===''?undefined:bannerUrl} className=" absolute h-full w-full" />
                         }
